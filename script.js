a071e525d0338cbf86154634aa819782d4e96b51
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-16 13:35:56 +0500

    ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let p = fetch("https://goweather.herokuapp.com/weather/lahore")
+let p = fetch("https://goweather.herokuapp.com/weather/peshawar")
 p.then((value1) =>{
 	return value1.json()
 }).then((value2) => {
